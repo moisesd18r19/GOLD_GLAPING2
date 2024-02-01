b6fd39283baa21e370f2679a2a9e24ffa541d31c
diomer2005 <diomer.quiroz.diossa@iemariacano.edu.co>
2024-02-01 10:50:26 -0500

    reserva

--- Glamping_Gold/pagos/models.py
+++ Glamping_Gold/pagos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Pago(models.Model):
-    fecha = models.DateField( default='True')
+    fecha = models.DateField( )
     metodo_pago = models.CharField(max_length=200)
-    valor = models.IntegerField( default='True')
+    valor = models.IntegerField( )
     status = models.BooleanField(default=True)
-    reserva = models.ForeignKey('reservas.Reserva', on_delete= models.DO_NOTHING, default='True')
+    reserva = models.ForeignKey('reservas.Reserva', on_delete= models.DO_NOTHING)
     
     def __str__(self):
         return self.reserva
