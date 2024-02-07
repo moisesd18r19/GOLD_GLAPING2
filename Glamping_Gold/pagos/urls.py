470d50a76cfacc625c9f13f386e23b8166e2e788
AlexSanchez24 <alex24sanchez2005@gmail.com>
2024-02-07 07:16:34 -0500

    Edit pago complete

--- Glamping_Gold/pagos/urls.py
+++ Glamping_Gold/pagos/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create/', views.create_pagos, name='create_pago'), 
     path('detail/<int:pago_id>/', views.detail_pago, name='detail_pago'),  
     path('delete/<int:pago_id>/', views.delete_pago, name='delete_pago'),
+    path('edit/<int:pago_id>/', views.edit_pago, name='edit_pago'),
         
 
  ]
